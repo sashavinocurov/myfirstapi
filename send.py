932d841a181a9ffb9261cf2ba08f0e8751ebf7e7
sashavinocurov <vinocurovxandr@gmail.com>
2019-10-22 17:55:22 -0700

    Programmers

--- send.py
+++ send.py
@@ -1,8 +1,8 @@
 import requests
 
 headers = {}
-headers['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcxNzg4OTQwLCJqdGkiOiIxZDdiOWQwNzIwMjk0NTAxYjQwYzBlZGFhNDkwZmQzMCIsInVzZXJfaWQiOjF9.Q_GXOAi4ZHBDQtuB_kJfpU8ELp__9grr3mYSIoNmO2M'
+headers['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcxNzkyMTU2LCJqdGkiOiI1MGNmY2QwYzcwYTU0M2E1OTk3OWM3YzkzODk4MjRkMiIsInVzZXJfaWQiOjF9.xwlgJP9wdM3DeLtPBjFktaYGE_t5iKez8Auob52wFZo'
 
-r = requests.get('http://localhost:8000/paradigms/', headers=headers)
+r = requests.get('http://localhost:8000/programmers/', headers=headers)
 
 print(r.text)
